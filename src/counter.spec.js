ed8bc4df3450a6edbbefafae1462c2b17ffaee06
speakingcode <dlissner@gmail.com>
2019-02-01 16:47:26 -0600

    Test counter conditions

--- src/counter.spec.js
+++ src/counter.spec.js
@@ -1,27 +1,39 @@
 import { counter } from './counter'
 
 describe('counter', () => {
-  it('should increment 0 to 1', () => {
+  it('increments 0 to 1', () => {
     expect(
       counter(0, {type: 'INCREMENT'})
     ).toEqual(1)
   })
 
-  it('should increment 1 to 2', () => {
+  it('increments 1 to 2', () => {
     expect(
       counter(1, {type: 'INCREMENT'})
     ).toEqual(2)
   })
 
-  it('should decrement 2 to 1', () => {
+  it('decrements 2 to 1', () => {
     expect(
       counter(2, {type: 'DECREMENT'})
     ).toEqual(1)
   })
 
-  it('should decrement 1 to 0', () => {
+  it('decrements 1 to 0', () => {
     expect(
       counter(1, {type: 'DECREMENT'})
+    ).toEqual(0)
+  })
+
+  it('ignores other actions', () => {
+    expect(
+      counter(1, {type: 'SOMETHING_ELSE'})
+    ).toEqual(1)
+  })
+
+  it('defaults to 0', () => {
+    expect(
+      counter(undefined, {})
     ).toEqual(0)
   })
 })
