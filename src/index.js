d72a886f86d83f5055acc92c6848e45c6f2446b2
speakingcode <dlissner@gmail.com>
2019-02-01 12:23:00 -0600

    Install and configure webpack, jest, npm scripts. Create initial test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDom rom 'react-dom'
+
+ReactDOM.render(
+  <div>ToDo</div>,
+  document.getElementById('app')
+)
---
+++ src/index.spec.js
@@ -0,0 +1,5 @@
+describe('Application setup', () => {
+  test('should work', () => {
+    expect(true)
+  })
+})
