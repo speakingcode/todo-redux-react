00ca96b338062fb105bb2d59c27a7522d1a3d3da
speakingcode <dlissner@gmail.com>
2019-03-04 09:10:12 -0600

    Refactor out addTodo

--- todo.js
+++ todo.js
@@ -1,19 +1,26 @@
 export const todos = (state = [], action) => {
+
   switch(action.type) {
     case 'ADD_TODO':
-      const {id, text} = action;
-      return [
-        ...state,
-        {
-          id,
-          text,
-          completed: false
-        }
-      ]
+      return addTodo(state, {
+        id: action.id,
+        text: action.text
+      })
     default:
       return state
   }
 }
+
+export const addTodo = (todos, todo) => {
+  return [
+    ...todos,
+    {
+      ...todo,
+      completed: false
+    }
+  ]
+}
+
 
 export const toggleTodo = (todo) => ({
   ...todo,
