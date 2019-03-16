10ab8140036d59abe5a0c9c441959ae487cbe14c
speakingcode <dlissner@gmail.com>
2019-03-16 11:45:01 -0500

    Use combineReducers for parent reducer composition

--- src/todoAppReducer.js
+++ src/todoAppReducer.js
@@ -1,9 +1,15 @@
 import { todos }      from './todo'
 import { todoFilter } from './todoFilter'
 
-export const todoAppReducer = (state = {}, action) => {
-  return {
-    todos      : todos(state.todos, action),
-    todoFilter : todoFilter(state.todoFiler, action)
-  }
-}
+import { combineReducers } from './combineReducers'
+
+export const todoAppReducer = combineReducers({
+  todos,
+  todoFilter
+})
+//export const todoAppReducer = (state = {}, action) => {
+//  return {
+//    todos      : todos(state.todos, action),
+//    todoFilter : todoFilter(state.todoFiler, action)
+//  }
+//}
